784d4c4c1f651b8b80ce6d74c30f6c1d6f36277e
llybood <llybood@yahoo.com>
2021-07-08 15:24:21 +0800

    添加zookeeper部署yaml文件

---
+++ zookeeper/uninstall_zookeeper_cluster.sh
@@ -0,0 +1,5 @@
+kubectl delete -f yaml/headless-service.yaml
+kubectl delete -f yaml/zk-service.yaml
+kubectl delete -f yaml/poddisruptionbudget.yaml
+kubectl delete -f yaml/zk-statefulset.yaml
+#kubectl delete -f yaml/namespace.yaml
