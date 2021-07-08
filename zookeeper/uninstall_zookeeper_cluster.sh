fcbdf9b6dc3ba055d86e15f5c29c90958315310a
llybood <llybood@yahoo.com>
2021-07-08 16:48:22 +0800

    修改卸载zookeeper脚本

--- zookeeper/uninstall_zookeeper_cluster.sh
+++ zookeeper/uninstall_zookeeper_cluster.sh
@@ -1,5 +1,4 @@
 kubectl delete -f yaml/headless-service.yaml
-kubectl delete -f yaml/zk-service.yaml
 kubectl delete -f yaml/poddisruptionbudget.yaml
 kubectl delete -f yaml/zk-statefulset.yaml
-#kubectl delete -f yaml/namespace.yaml
+kubectl delete -f yaml/namespace.yaml
