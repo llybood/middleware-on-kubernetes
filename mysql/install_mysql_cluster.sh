45a8b595e966b2f9dc562bd0b56a1e6d3e61c5f1
llybood <llybood@yahoo.com>
2021-08-13 14:33:59 +0800

    优化mysql部署yaml文件

--- mysql/install_mysql_cluster.sh
+++ mysql/install_mysql_cluster.sh
@@ -1,3 +1,4 @@
+kubectl apply -f ../storage/local-path-storage.yaml
 kubectl apply -f yaml/mysql-configmap.yaml -n mysql
 kubectl apply -f yaml/mysql-svc.yaml -n mysql
 kubectl apply -f yaml/mysql-statefulset.yaml -n mysql
