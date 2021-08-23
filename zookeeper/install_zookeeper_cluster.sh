67d74628c35373f0824be75fc9390f46f311a40e
llybood <llybood@yahoo.com>
2021-08-23 15:37:55 +0800

    删除zk的services部署文件

--- zookeeper/install_zookeeper_cluster.sh
+++ zookeeper/install_zookeeper_cluster.sh
@@ -1,7 +1,6 @@
 kubectl apply -f yaml/namespace.yaml
 kubectl apply -f yaml/local-path-storage.yaml
 kubectl apply -f yaml/headless-service.yaml
-kubectl apply -f yaml/zk-service.yaml
 kubectl apply -f yaml/poddisruptionbudget.yaml
 kubectl apply -f yaml/zk-statefulset.yaml
 
