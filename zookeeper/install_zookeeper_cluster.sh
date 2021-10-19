cceb6792620d40b49c4bdbe92b6d204db1bcbf7b
llybood <llybood@yahoo.com>
2021-10-19 17:39:07 +0800

    修改zookeeper部署文件

--- zookeeper/install_zookeeper_cluster.sh
+++ zookeeper/install_zookeeper_cluster.sh
@@ -1,5 +1,5 @@
 kubectl apply -f yaml/namespace.yaml
-kubectl apply -f yaml/local-path-storage.yaml
+kubectl apply -f ../storage/local-path-storage.yaml
 kubectl apply -f yaml/headless-service.yaml
 kubectl apply -f yaml/poddisruptionbudget.yaml
 kubectl apply -f yaml/zk-statefulset.yaml
