8eb13955759bddb147f7c5fea4ca36269f0cfb8d
llybood <llybood@yahoo.com>
2021-10-20 15:34:29 +0800

    修改mysql部署文件

--- mysql/install_mysql_cluster.sh
+++ mysql/install_mysql_cluster.sh
@@ -1,4 +1,5 @@
 kubectl apply -f ../storage/local-path-storage.yaml
+kubectl create ns mysql
 kubectl apply -f yaml/mysql-configmap.yaml -n mysql
 kubectl apply -f yaml/mysql-svc.yaml -n mysql
 kubectl apply -f yaml/mysql-statefulset.yaml -n mysql
--- mysql/uninstall_mysql_cluster.sh
+++ mysql/uninstall_mysql_cluster.sh
@@ -1,3 +1,4 @@
 kubectl delete -f yaml/mysql-configmap.yaml -n mysql
 kubectl delete -f yaml/mysql-svc.yaml -n mysql
 kubectl delete -f yaml/mysql-statefulset.yaml -n mysql
+kubectl delete ns mysql
