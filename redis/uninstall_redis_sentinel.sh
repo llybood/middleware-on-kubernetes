596d85715b5a7c9fc7770b89266ea60a79eed897
llybood <llybood@yahoo.com>
2021-10-20 17:34:48 +0800

    删除脚本增加删除命名空间步骤

--- redis/uninstall_redis_sentinel.sh
+++ redis/uninstall_redis_sentinel.sh
@@ -3,3 +3,4 @@ kubectl delete -f yaml/redis-sentinel/redis-master-svc.yaml -n redis
 kubectl delete -f yaml/redis-sentinel/redis-sentinel-svc.yaml -n redis
 kubectl delete -f yaml/redis-sentinel/redis-master-deployment.yaml -n redis
 kubectl delete -f yaml/redis-sentinel/redis-slave-statefulset.yaml -n redis
+kubectl delete ns redis
