e2ab556325b892c21ae163f379c0d1db56612f7f
llybood <llybood@yahoo.com>
2021-11-24 12:54:54 +0800

    修改prometheus安装脚本

--- prometheus/install_prometheus.sh
+++ prometheus/install_prometheus.sh
@@ -1,4 +1,5 @@
 kubectl apply -f yaml/crds
 until kubectl get servicemonitors --all-namespaces ; do date; sleep 1; echo ""; done
+kubectl create secret generic additional-configs --from-file=prometheus-additional.yaml -n monitoring
 kubectl apply -f yaml/
 kubectl apply -f yaml/service/
