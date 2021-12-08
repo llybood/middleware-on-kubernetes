63843ec66b33b4c64664688ab5bcd668813f134a
llybood <llybood@yahoo.com>
2021-12-08 17:32:07 +0800

    把compactor独立出来部署

--- loki/install_loki_cluster.sh
+++ loki/install_loki_cluster.sh
@@ -1,12 +1,14 @@
 #!/bin/sh 
 kubectl create ns loki
 kubectl create secret generic loki-config --from-file=config/loki.yaml -n loki
-# 部署loki
+# 部署loki-write
 kubectl apply -f yaml/write-cluster/statefulset.yaml -n loki
 kubectl apply -f yaml/write-cluster/service.yaml -n loki
-# 部署loki-frontend
+# 部署loki-read(querier和query-frontend)
 kubectl apply -f yaml/read-cluster/statefulset.yaml -n loki
 kubectl apply -f yaml/read-cluster/service.yaml -n loki
+# 部署compactor
+kubectl apply -f yaml/compactor/deployment.yaml -n loki
 # 部署promtail
 kubectl create secret generic promtail-config --from-file=config/promtail.yaml -n loki
 kubectl apply -f yaml/promtail/rbac.yaml -n loki
--- loki/uninstall_loki_cluster.sh
+++ loki/uninstall_loki_cluster.sh
@@ -3,6 +3,7 @@ kubectl delete -f yaml/read-cluster/statefulset.yaml -n loki
 kubectl delete -f yaml/read-cluster/service.yaml -n loki
 kubectl delete -f yaml/write-cluster/statefulset.yaml -n loki
 kubectl delete -f yaml/write-cluster/service.yaml -n loki
+kubectl delete -f yaml/compactor/deployment.yaml -n loki
 kubectl delete secret loki-config -n loki 
 kubectl delete secret promtail-config -n loki
 kubectl delete -f yaml/promtail/daemonset.yaml -n loki
