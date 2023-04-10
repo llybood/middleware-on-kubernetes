6df8cad477c1c801e0f811d9ddf8a733e4042fac
llybood <llybood@yahoo.com>
2023-04-10 17:26:24 +0800

    优化dolphinscheduler部署文件

--- dolphinscheduler/install.sh
+++ dolphinscheduler/install.sh
@@ -1,18 +1,30 @@
+#!/bin/bash
+namespace="dolphinscheduler"
+
+# 创建namespace
+kubectl create namespace dolphinscheduler
+
+# 创建rbac
+kubectl apply -f yaml/rbac.yaml -n $namespace
+
 # 创建configmap
-kubectl apply -f yaml/config/ -n test
+kubectl apply -f yaml/config/ -n $namespace
 
 # 初始化数据
-kubectl apply -f yaml/tools/job.yaml -n test
-until kubectl get pods -n test | grep db-init-job | grep Completed; do date; sleep 5; echo "正在初始化数据"; done
+kubectl apply -f yaml/tools/job.yaml -n $namespace
+until kubectl get pods -n $namespace | grep db-init-job | grep Completed; do date; sleep 5; echo "正在初始化数据"; done
 
 # 部署master
-kubectl apply -f yaml/master-server/statefulset.yaml -n test
+kubectl apply -f yaml/master-server/statefulset.yaml -n $namespace
 
 # 部署worker
-kubectl apply -f yaml/worker-server/statefulset.yaml -n test
+kubectl apply -f yaml/worker-server/statefulset.yaml -n $namespace
 
 # 部署api-server
-kubectl apply -f yaml/api-server/deployment.yaml -n test
+kubectl apply -f yaml/api-server/deployment.yaml -n $namespace
 
 # 部署alert-server
-kubectl apply -f yaml/alert-server/deployment.yaml -n test
+kubectl apply -f yaml/alert-server/deployment.yaml -n $namespace
+
+# 部署service
+kubectl apply -f yaml/service/clusterip.yaml -n $namespace
--- dolphinscheduler/uninstall.sh
+++ dolphinscheduler/uninstall.sh
@@ -1,17 +1,28 @@
+#!/bin/bash
+namespace="dolphinscheduler"
+
+# 删除rbac
+kubectl delete -f yaml/rbac.yaml -n $namespace
 # 删除configmap
-kubectl delete -f yaml/config/ -n test
+kubectl delete -f yaml/config/ -n $namespace
 
 # 删除初始化数据job
-kubectl delete -f yaml/tools/job.yaml -n test
+kubectl delete -f yaml/tools/job.yaml -n $namespace
 
 # 删除master
-kubectl delete -f yaml/master-server/statefulset.yaml -n test
+kubectl delete -f yaml/master-server/statefulset.yaml -n $namespace
 
 # 删除worker
-kubectl delete -f yaml/worker-server/statefulset.yaml -n test
+kubectl delete -f yaml/worker-server/statefulset.yaml -n $namespace
 
 # 删除api-server
-kubectl delete -f yaml/api-server/deployment.yaml -n test
+kubectl delete -f yaml/api-server/deployment.yaml -n $namespace
 
 # 删除alert-server
-kubectl delete -f yaml/alert-server/deployment.yaml -n test
+kubectl delete -f yaml/alert-server/deployment.yaml -n $namespace
+
+# 删除service
+kubectl delete -f yaml/service/clusterip.yaml -n $namespace
+
+# 删除namespace
+kubectl delete namespace $namespace
